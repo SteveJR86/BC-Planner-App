2f4fd65dc77e8f9feefe635fbb0976221b076769
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2023-01-29 07:03:05 +0000

    style updated to center align timeblock and code for local storage added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 // check localstorage to see if data for day is already saved
 // update timeblocks with data from local storage if appropriate
-// update background color of timeblocks for current timeblock and future timeblocks
+
+// global variable for current date
+let currentDate = moment();
 
 function renderRow(hourDisplay){
   // function to render a single row in the timeblock taking a moment.js object as the hour which
@@ -11,6 +13,8 @@ function renderRow(hourDisplay){
   // description alters which classes are added depending on whether the hour is past, present
   // or future
   let description = $('<textarea cols="35">')
+  // add id for saving later
+  description.attr('id', hourDisplay.format('H'));
   description.addClass('description');
   let currentTime = moment().set({minutes: 0, seconds: 0, milliseconds: 0});
   // if else block to check if hour of current row is before, equal to or after the current time
@@ -22,6 +26,8 @@ function renderRow(hourDisplay){
     description.addClass('future');
   }
   let saveBtn = $('<i>');
+  // add data attribute to identify which element is clicked
+  saveBtn.attr('data-hour', hourDisplay.format('H'));
   saveBtn.addClass('fas fa-save saveBtn');
   let row = $('<div>');
   row.addClass('row');
@@ -39,11 +45,46 @@ function renderTimeBlock(startTime, endTime){
   }
 }
 
+function saveDetails(event){
+  // function to save details when save icon is clicked
+  // only act on clicking <i> element
+  if(event.target.nodeName === 'I'){
+    // get which hour was clicked and turn it into ID name
+    let clickedHour = event.target.getAttribute('data-hour');
+    let clickedHourId = "#" + clickedHour;
+    // get text from textarea
+    let textToSave = $(clickedHourId).val();
+    // get current localstorage data
+    let plannerData = window.localStorage.getItem('plannerData');
+    plannerData = JSON.parse(plannerData);
+    // check if data exists and is for today
+    if(!plannerData || plannerData.date!=currentDate.format('DD-MM-YYYY')){
+      plannerData = {};
+      plannerData.date = currentDate.format('DD-MM-YYYY');
+    }
+    plannerData[clickedHour] = textToSave;
+    window.localStorage.setItem('plannerData', JSON.stringify(plannerData));
+  }
+}
+
+function loadDetails(){
+  // function to load saved data into timeblocks if present
+  let plannerData = window.localStorage.getItem('plannerData');
+  plannerData = JSON.parse(plannerData);
+  if(plannerData.date === currentDate.format('DD-MM-YYYY')){
+    jQuery.each($('textarea'), function(i){
+      let currentHourRow = $('textarea')[i];
+      $('#' + currentHourRow.getAttribute('id')).val(plannerData[currentHourRow.getAttribute('id')]);
+    });
+  }
+}
+
 $(document).ready(function(){
   // set current date at top of page
-  let currentDate = moment();
   $('#currentDay').text(currentDate.format('dddd, MMMM Do'));
-  console.log(moment().set({hour: 9}).format('ha, dddd, MMMM Do'));
   // render time block area with rows as required
   renderTimeBlock(9, 17);
+  loadDetails();
 });
+
+$('.time-block').on('click', saveDetails);
